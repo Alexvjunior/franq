0b54fc8ba26e63268092b9856be8397e96108ac3
Alexsander Junior <alexsander.junior@luizalabs.com>
2023-05-23 20:21:23 -0300

    Add modal form

--- apps/leads/service.py
+++ apps/leads/service.py
@@ -35,7 +35,3 @@ class LeadService:
         except Exception as e:
             print(e)
             return None
-
-    def create_leads_local(self, name, email, phone, interest):
-        Lead.objects.create(name=name, email=email,
-                            phone=phone, interest=interest)
